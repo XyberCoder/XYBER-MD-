62003c3517cd6270246de7c84750fb6f127f0b2f
XyberCoder <xybercoder144@gmail.com>
2025-05-26 04:00:29 +0200

    Update set.js

--- set.js
+++ set.js
@@ -1,10 +1,10 @@
 global.prefix = '/'; // Set Your Preferred Prefix
 global.botName = 'CYBER-MD'; // Bot Name
 global.botVersion = '2.0.0';
-global.botPic = 'https://files.giftedtech.web.id/file/gifted-md.jpg'; // Can use path, can also use link
+global.botPic = 'https://imgur.com/a/gdVbKkj'; // Can use path, can also use link
 global.botToken = '7920911651:AAGrxVZYr28gaoElacKei3udJ9laqFjcUao'; // Replace With Your Bot Token (Get it from BotFather)
 global.ownerUsername = 'CyberCoder'; // Replace With Your Telegram Username(For Bot Owner)
-global.timeZone = 'Africa/Nairobi'; // Your Time Zone
+global.timeZone = 'Africa/Mutare'; // Your Time Zone
 global.ownerId = [263718228938]; // Replace With Your Owner ID(For Bot Owner), You Can Add More than 1
 global.footer = '*Powered by CyberCoder*';
 global.ytdlWeb = 'https://ytdl.giftedtech.web.id';
@@ -15,8 +15,8 @@ global.maherApi = 'https://api.nexoracle.com'; // NexOracle Api
 global.maherKey = ''; // NexOracle Api Key
 global.lyffeApi = 'https://levanter.onrender.com'; // Lyffe Api
 global.giftedWaChannel = 'https://whatsapp.com/channel/0029Vb68H1g1CYoVlRmJ0r3c';
-global.giftedRepo = "https://github.com/mauricegift/telegram-bot";
-global.giftedApiRepo = "https://api.github.com/repos/mauricegift/telegram-bot";
+global.giftedRepo = "https://github.com/XyberCoder";
+global.giftedApiRepo = "https://github.com/XyberCoder";
 
 global.giftechMess = {
     internalerr: 'Internal Server Error!',
